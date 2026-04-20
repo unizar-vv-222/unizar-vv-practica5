d9a9a5bcb47e6c16f3bae6c5b407f1010b549ade
Davih <897745@unizar.es>
2026-04-20 11:39:47 +0200

    Carga inicial

---
+++ GildedRose0/src/test/java/com/alexaitken/gildedrose/InventoryTest.java
@@ -0,0 +1,283 @@
+package com.alexaitken.gildedrose;
+
+
+import com.alexaitken.gildedrose.Inventory;
+import com.alexaitken.gildedrose.Item;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class InventoryTest {
+
+    private Inventory createInventory(Item... items) {
+        return new Inventory(items);
+    }
+
+    //P3
+    @Test
+    public void should_never_changes_quailty_of_Sulfuras() {
+        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        Inventory inventory = createInventory(sulfuras);
+        inventory.updateQuality();
+        assertEquals(80, sulfuras.getQuality());
+    }
+
+    //P3
+    @Test
+    public void should_never_changes_sellIn_of_Sulfuras() {
+        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        Inventory inventory = createInventory(sulfuras);
+        inventory.updateQuality();
+        assertEquals(0, sulfuras.getSellIn());
+    }
+
+    //P9
+    @Test
+    public void should_lower_the_sellIn_by_one_for_normal_items() {
+        Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
+        Inventory inventory = createInventory(normalItem);
+        inventory.updateQuality();
+        assertEquals(9, normalItem.getSellIn());
+    }
+
+    //P9
+    @Test
+    public void should_lower_the_quality_by_one_for_normal_items() {
+        Item normalItem = new Item("+5 Dexterity Vest", 10, 20);
+        Inventory inventory = createInventory(normalItem);
+        inventory.updateQuality();
+        assertEquals(19, normalItem.getQuality());
+    }
+
+    //P9b
+    @Test
+    public void should_not_lower_the_quality_below_zero() {
+        Item normalItem = new Item("+5 Dexterity Vest", 10, 0);
+        Inventory inventory = createInventory(normalItem);
+        inventory.updateQuality();
+        assertEquals(0, normalItem.getQuality());
+    }
+
+    //P10
+    @Test
+    public void should_lower_the_quality_twice_as_fast_once_the_sell_in_date_has_passed() {
+        Item normalItem = new Item("+5 Dexterity Vest", -1, 25);
+        Inventory inventory = createInventory(normalItem);
+        inventory.updateQuality();
+        assertEquals(23, normalItem.getQuality());
+    }
+
+    //P1
+    @Test
+    public void should_increase_the_quality_of_aged_brie_as_it_gets_older() {
+        Item agedBrie = new Item("Aged Brie", 10, 25);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(26, agedBrie.getQuality());
+    }
+
+    //P1b
+    @Test
+    public void should_not_increase_the_quality_of_aged_brie_over_50() {
+        Item agedBrie = new Item("Aged Brie", 10, 50);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(50, agedBrie.getQuality());
+    }
+
+    //P8
+    @Test
+    public void should_lower_backstage_passes_to_zero_quality_once_concert_has_happened() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(0, backStagePass.getQuality());
+    }
+
+    //P4
+    @Test
+    public void should_increase_backstage_passes_quality_by_1_when_the_concert_is_more_than_10_days_away() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(21, backStagePass.getQuality());
+    }
+
+    //P5
+    @Test
+    public void should_increase_backstage_passes_quality_by_2_when_the_concert_is_10_days_or_less_away() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 27);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(29, backStagePass.getQuality());
+    }
+
+    //P6
+    @Test
+    public void should_increase_backstage_passes_quality_by_3_when_the_concert_is_5_days_or_less_away() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 44);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(47, backStagePass.getQuality());
+    }
+
+    //P4b, P5c, P6d
+    @Test
+    public void should_not_increase_backstage_passes_above_a_quality_of_50() {
+        Item backStagePassMoreThan10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50);
+        Item backStagePass10DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+        Item backStagePass5DaysAway = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48);
+        Inventory inventory = createInventory(backStagePassMoreThan10DaysAway, backStagePass10DaysAway, backStagePass5DaysAway);
+        inventory.updateQuality();
+        assertEquals(50, backStagePassMoreThan10DaysAway.getQuality());
+        assertEquals(50, backStagePass10DaysAway.getQuality());
+        assertEquals(50, backStagePass5DaysAway.getQuality());
+    }
+
+
+
+    // ----------------------------------------------------------------
+    // PRUEBAS QUE FALTABAN AL PRINCIPIO
+    // ----------------------------------------------------------------
+
+    //P1
+    @Test
+    public void p1_shellIn() {
+        Item agedBrie = new Item("Aged Brie", 10, 25);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(9, agedBrie.getSellIn());
+    }
+
+    //P1b
+    @Test
+    public void p1b_shellIn() {
+        Item agedBrie = new Item("Aged Brie", 10, 50);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(9, agedBrie.getSellIn());
+    }
+
+    //P2
+    @Test
+    public void p2_shellIn() {
+        Item agedBrie = new Item("Aged Brie", -8, 50);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(-9, agedBrie.getSellIn());
+    }
+
+    //P2
+    @Test
+    public void p2_quality() {
+        Item agedBrie = new Item("Aged Brie", -8, 25);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(27, agedBrie.getQuality());
+    }
+
+    //P2b
+    @Test
+    public void p2b_shellIn() {
+        Item agedBrie = new Item("Aged Brie", -8, 50);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(-9, agedBrie.getSellIn());
+    }
+
+    //P2b
+    @Test
+    public void p2b_quality() {
+        Item agedBrie = new Item("Aged Brie", -8, 50);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(50, agedBrie.getQuality());
+    }
+
+    //P2c
+    @Test
+    public void p2c_shellIn() {
+        Item agedBrie = new Item("Aged Brie", -8, 49);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(-9, agedBrie.getSellIn());
+    }
+
+    //P2c
+    @Test
+    public void p2c_quality() {
+        Item agedBrie = new Item("Aged Brie", -8, 49);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(50, agedBrie.getQuality());
+    }
+
+    //P2d
+    @Test
+    public void p2d_shellIn() {
+        Item agedBrie = new Item("Aged Brie", 0, 25);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(-1, agedBrie.getSellIn());
+    }
+
+    //P2d
+    @Test
+    public void p2d_quality() {
+        Item agedBrie = new Item("Aged Brie", 0, 25);
+        Inventory inventory = createInventory(agedBrie);
+        inventory.updateQuality();
+        assertEquals(27, agedBrie.getQuality());
+    }
+
+    //P4
+    @Test
+    public void p4_shellIn() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(10, backStagePass.getSellIn());
+    }
+
+    //P4b
+    @Test
+    public void p4b_shellIn() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(10, backStagePass.getSellIn());
+    }
+
+    //P5
+    @Test
+    public void p5_shellIn() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 27);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(8, backStagePass.getSellIn());
+    }
+
+    //P5b
+    @Test
+    public void p5b_shellIn() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 27);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(9, backStagePass.getSellIn());
+    }
+
+    //P5c
+    @Test
+    public void p5c_quality() {
+        Item backStagePass = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+        Inventory inventory = createInventory(backStagePass);
+        inventory.updateQuality();
+        assertEquals(50, backStagePass.getQuality());
+    }
+
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+## Verificación y Validación
+# Práctica 5: Depuración y análisis estático de código
+
+Este repositorio contiene un proyecto IntelliJ con tres módulos distintos 
+(`GildedRose0`, `GildedRose1` y `GildedRose2`), correspondientes a la 
+_kata_ de refactorización Gilded Rose. Este repositorio está basado en la versión
+de Java preparada por Alex Aitken (https://github.com/alexaitken/GildedRose_java)
+de la kata original de [Terry Hughes](https://twitter.com/TerryHughes) y
+[Bobby Johnson](https://twitter.com/NotMyself) para C#
+(https://github.com/NotMyself/GildedRose).
+ 
+Los proyectos `GildedRose1` y `GildedRose2` tienen un defecto cada uno.
